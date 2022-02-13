a3766c6fef0eea8a4c4190cd24041873f4483742
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-02-13 18:27:09 -0400

    First part

---
+++ src/components/CrudForm.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+const initialForm = {
+    name:"",
+    constellation: "",
+    id: null,
+}
+
+ const CrudForm = () => {
+
+    const [form, setForm] = useState(initialForm)
+
+const handleChange = (e)=>{} 
+const handleSubmit = (e)=>{} 
+const handleReset = (e)=>{} 
+
+
+  return (
+  <div>
+      <h3>Agregar</h3>
+      <form onSubmit={handleSubmit}>
+          <input type="text" name="name" placeholder="Nombre" onChange={handleChange} value={form.name}/>
+          <input type="text" name="constellation" placeholder="Constelacion" onChange={handleChange} value={form.constellation}/>
+          <input type= "submit" value="Enviar"/>
+          <input type= "reset" value="Limpiar" onClick={handleReset}/>
+      </form>
+  </div>
+  );
+};
+export default CrudForm;
