850f46c50f67500b4c9c6c0db383223c2b870a38
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-02-17 22:47:59 -0400

    SongSearch apis

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import Loader from "./Loader";
 import SongDetails from "./SongDetails";
 import SongForm from "./SongForm";
@@ -9,8 +10,33 @@ const SongSearch = () => {
   const [bio, setBio] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    if (search === null) return;
+
+    const fetchData = async () => {
+      const { artist, song } = search;
+
+      let artistUrl = `theaudiodb.com/api/v1/json/2/search.php?s=${artist}`;
+      let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+      console.log(artistUrl, songUrl);
+
+      setLoading(true);
+      const [artistRes, songRes] = await Promise.all([
+        helpHttp().get(artistUrl),
+        helpHttp().get(songUrl),
+      ]);
+      console.log(artistRes, songRes);
+
+      setBio(artistRes);
+      setLyric(songRes);
+      setLoading(false);
+    };
+    fetchData();
+  }, [search]);
+
   const handleSearch = (data) => {
-    console.log(data);
+    //console.log(data);
     setSearch(data);
   };
 
