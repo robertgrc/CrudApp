5d4569a239faa42f3446625bccfb4b37a463046b
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-02-18 19:15:27 -0400

    Selects anidados first part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 // import CrudApi from "./components/CrudApi";
 import CrudApp from "./components/CrudApp";
+import SelectAnidados from "./components/SelectAnidados";
 import SongSearch from "./components/SongSearch";
 
 function App() {
   return (
     <div>
       <h1>Ejercicios con React</h1>
+      <hr />
+
+      <SelectAnidados />
       <hr />
       <SongSearch />
       {/* <CrudApi /> */}
---
+++ src/components/SelectList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SelectList = () => {
+  return (
+    <div>
+      <select name="" id="">
+        <option value="">---</option>
+      </select>
+    </div>
+  );
+};
+export default SelectList;
